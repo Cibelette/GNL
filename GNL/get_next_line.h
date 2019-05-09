2ed9f005ff867fa9eb1e2f4725ba865d1f58d406
Clara LAGIER <clagier@e2r5p13.42.fr>
2019-05-09 17:25:46 +0200

    ne boucle plus a l'infini

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: clagier <clagier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 15:18:19 by clagier           #+#    #+#             */
-/*   Updated: 2019/05/09 17:11:19 by clagier          ###   ########.fr       */
+/*   Updated: 2019/05/09 17:25:30 by clagier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NETXT_LINE_H
 # define GET_NETXT_LINE_H
 
-# define BUFF_SIZE 50
+# define BUFF_SIZE 99
 
 int get_next_line(const int fd, char **line);
 
